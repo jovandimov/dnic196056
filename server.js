2f6b6e3aa3fe71a9816def42662ac2bf017f258b
Jovan Dimov <jovandimov@gmail.com>
2022-09-30 20:15:11 +0200

    n3

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/dnic-frontend'));
+app.use(express.static('./dnic-frontend'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/dnic-frontend/'}),
+  res.sendFile('index.html', {root: '/dnic-frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
